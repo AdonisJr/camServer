7ba2d888d339a4d8a7e0395d239330f547cb085a
unknown <adonisjr.suico@gmail.com>
2024-01-16 18:12:03 +0800

    Multiple Changes

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/officer', require('../controller/officer/index'));
+router.use('/user', require('../controller/user/index'));
 router.use('/person', require('../controller/personOfConcern/index'));
 router.use('/crime', require('../controller/crime/index'));
-
+router.use('/fileUpload', require('../controller/fileUpload/index'));
+router.use('/history', require('../controller/history/index'));
+router.use('/personHistory', require('../controller/personHistory/index'));
 module.exports = router;
